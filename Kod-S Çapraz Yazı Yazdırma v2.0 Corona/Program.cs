a5b939ba4e00deda0c65aafe4be4cf3352cad8c0
Murat Bilginer <mrtblgnr@muratbilginer.net>
2016-04-22 22:44:47 +0300

    2 Kod-S C# Proje Çapraz Yazı Yazdırma v2.0 Corona
    Çapraz Yazı Yazdırma programının Corona versiyonu.

---
+++ Kod-S Çapraz Yazı Yazdırma v2.0 Corona/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kod_S_Çapraz_Yazı_Yazdırma_v2._0_Corona
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string kelime;
+            int adet;
+
+            Console.Write("Yazılmasını istediğiniz kelimeyi yazınız:");
+            kelime = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Kaç Kere Yazılmasını İstiyorsunuz:");
+            adet = int.Parse(Console.ReadLine());
+
+
+            for (int i = 1; i <= adet; i++)
+            {
+                Console.WriteLine("{0," + (i + kelime.Length - 1) + "}", kelime);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
